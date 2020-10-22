3c0ebe67163a88e38f94e103feb0e9be80c206dc
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-10-22 10:39:29 +0200

    Add Controller for upload

---
+++ Lego.Server.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lego.Server.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
