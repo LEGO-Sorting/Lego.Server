19a6c5aff9b07cfa502e07b8a562ada8d58ea60f
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-10-25 15:52:45 +0100

    Add uploading files with test

--- tests/Lego.Server.IntegrationTests/UploadFiles.cs
+++ tests/Lego.Server.IntegrationTests/UploadFiles.cs
@@ -1,14 +1,39 @@
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Lego.Server.IntegrationTests.Infrastructure;
+using Lego.Server.WebApi;
+using Lego.Server.WebApi.Dto;
+using Shouldly;
 using Xunit;
 
 namespace Lego.Server.IntegrationTests
 {
-    public class UploadFiles
+    public class UploadFiles : IClassFixture<IntegrationTestsFixture<Startup>>
     {
-        [Fact]
-        public async Task UploadFiles_WhenNoVideoIncluded_ShouldReturn400()
+        private readonly IntegrationTestsFixture<Startup> _fixture;
+
+        public UploadFiles(IntegrationTestsFixture<Startup> fixture)
         {
+            _fixture = fixture;
+        }
+        
+        [Fact]
+        public async Task UploadFiles_WhenVideoIncluded_ShouldReturn200()
+        {
+            // Arrange
+            var body = new VideoFile()
+            {
+                Base64File = _fixture.ConvertVideoToBase64()
+            };
+
+            // Act
+            var result =
+                await _fixture.Client.PostAsJsonAsync(new Uri("/api/upload/files", UriKind.RelativeOrAbsolute), body);
             
+            // Assert
+            result.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
     }
 }
