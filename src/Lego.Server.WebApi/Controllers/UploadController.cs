9f192026ba61634060f3ca0a24c125358374d3b7
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2020-10-26 17:48:53 +0100

    Add communication between client and server

--- src/Lego.Server.WebApi/Controllers/UploadController.cs
+++ src/Lego.Server.WebApi/Controllers/UploadController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using Lego.Server.WebApi.Dto;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lego.Server.WebApi.Controllers
@@ -32,12 +34,12 @@ namespace Lego.Server.WebApi.Controllers
             return Ok(new {info.CanAccess});
         }
 
+        [Consumes("multipart/form-data")]
         [HttpPost("files")]
-        public async Task<IActionResult> Post([FromBody]VideoFile file)
+        public async Task<IActionResult> Post([FromForm]List<IFormFile> file)
         {
-            var convertedFile = Convert.FromBase64String(file.Base64File);
             // Get the file from the POST request
-            // var theFile = HttpContext.Request.Form.Files.GetFile("file");
+            var theFile = HttpContext.Request.Form.Files.GetFile("file");
 
             // Get the server path, wwwroot
             string webRootPath = _webHostEnvironment.WebRootPath;
@@ -46,11 +48,13 @@ namespace Lego.Server.WebApi.Controllers
             var fileRoute = Path.Combine(webRootPath, "uploads");
 
             // Get the mime type
-            // var mimeType = HttpContext.Request.Form.Files.GetFile("file").ContentType;
+            var mimeType = HttpContext.Request.Form.Files.GetFile("file").ContentType;
 
             // Get File Extension
+            string extension = System.IO.Path.GetExtension(theFile.FileName);
+
             // Generate Random name.
-            string name = $"{Guid.NewGuid().ToString()}.mp4";
+            string name = $"{Guid.NewGuid().ToString()}{extension}";
 
             // Build the full path inclunding the file name
             string link = Path.Combine(fileRoute, name);
@@ -67,7 +71,8 @@ namespace Lego.Server.WebApi.Controllers
             try
             {
                 // Copy contents to memory stream.
-                var stream = new MemoryStream(convertedFile);
+                var stream = new MemoryStream();
+                theFile.CopyTo(stream);
                 stream.Position = 0;
                 var serverPath = link;
 
